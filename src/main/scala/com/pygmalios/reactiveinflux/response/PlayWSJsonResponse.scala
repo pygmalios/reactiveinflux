d01903fde6a4714095d6c423237a258af7e15f9c
Rado Buransky <radoburansky@gmail.com>
2016-03-09 12:40:25 +0100

    PD-136: PlayWSJsonResponseSpec error processing

--- src/main/scala/com/pygmalios/reactiveinflux/response/PlayWSJsonResponse.scala
+++ src/main/scala/com/pygmalios/reactiveinflux/response/PlayWSJsonResponse.scala
@@ -13,45 +13,67 @@ abstract class PlayWSJsonResponse[+T](wsResponse: WSResponse) extends ReactiveIn
 
   protected def results: Seq[JsObject] = {
     wsResponse.status match {
-      case Status.OK =>
-        try {
-          // Get body as JSON
-          wsResponse.json match {
-            // Get it as JSON object
-            case jsObject: JsObject =>
-              // Get "results" field
-              jsObject \ resultsField match {
-                case JsDefined(resultsArray: JsArray) =>
-                  // Process all result item
-                  val results = resultsArray.value.map {
-                      // Get result item as JSON object
-                      case result: JsObject => result
-                      case other =>
-                        throw new ReactiveInfluxException(s"Ivalid result item! [$other]")
-                  }
-
-                  // Get and process errors from results (if any)
-                  processErrors(errorsFromResults(results))
-
-                  results
-                case _ =>
-                  throw new ReactiveInfluxException("No results JSON field!")
-              }
-            case _ =>
-              throw new ReactiveInfluxException(s"Not a JSON object!")
-          }
-        }
-        catch {
-          case ex: Exception =>
-            throw new ReactiveInfluxException(s"Invalid JSON response! [$wsResponse]", ex)
-        }
-
-      case _ =>
-        throw new ReactiveInfluxException(s"Not a successful response! [$wsResponse]")
+      case Status.OK => handleOk(wsResponse)
+      case _ => handleNotOk(wsResponse)
     }
   }
 
   protected def errorHandler: PartialFunction[ReactiveInfluxError, Option[ReactiveInfluxError]] = PartialFunction.empty
+
+  private def handleOk(wsResponse: WSResponse): Seq[JsObject] = {
+    try {
+      // Get body as JSON
+      wsResponse.json match {
+        // Get it as JSON object
+        case jsObject: JsObject =>
+          // Get "results" field
+          jsObject \ resultsField match {
+            case JsDefined(resultsArray: JsArray) =>
+              // Process all result item
+              val results = resultsArray.value.map {
+                // Get result item as JSON object
+                case result: JsObject => result
+                case other =>
+                  throw new ReactiveInfluxException(s"Ivalid result item! [$other]")
+              }
+
+              // Get and process errors from results (if any)
+              processErrors(errorsFromResults(results))
+
+              results
+            case _ =>
+              throw new ReactiveInfluxException("No results JSON field!")
+          }
+        case _ =>
+          throw new ReactiveInfluxException(s"Not a JSON object!")
+      }
+    }
+    catch {
+      case ex: Exception =>
+        throw new ReactiveInfluxException(s"Invalid JSON response! [$wsResponse]", ex)
+    }
+  }
+
+  private def handleNotOk(wsResponse: WSResponse): Seq[JsObject] = {
+    try {
+      // Try to get JSON errors
+      wsResponse.json match {
+        case jsObject: JsObject =>
+          // Get "error" field
+          jsObject \ errorField match {
+            case JsDefined(reason: JsString) =>
+              // Process errors
+              processErrors(Seq(reason.value))
+            case _ =>
+          }
+        case _ =>
+      }
+      Nil
+    }
+    finally {
+      throw new ReactiveInfluxException(s"Not a successful response! [$wsResponse]")
+    }
+  }
 
   private def processErrors(stringErrors: Iterable[String]): Unit = {
     val errors = stringErrors.map(ReactiveInfluxError.apply).flatMap(errorHandler.applyOrElse(_, defaultErrorHandler)).toSet
--- src/test/scala/com/pygmalios/reactiveinflux/response/PlayWSJsonResponseSpec.scala
+++ src/test/scala/com/pygmalios/reactiveinflux/response/PlayWSJsonResponseSpec.scala
@@ -1,5 +1,7 @@
 package com.pygmalios.reactiveinflux.response
 
+import com.pygmalios.reactiveinflux.ReactiveInfluxException
+import com.pygmalios.reactiveinflux.error.ReactiveInfluxError
 import org.junit.runner.RunWith
 import org.scalatest.FlatSpec
 import org.scalatest.junit.JUnitRunner
@@ -9,11 +11,13 @@ import play.api.libs.json.{JsString, JsArray, Json, JsObject}
 import play.api.libs.ws.WSResponse
 import org.mockito.Mockito._
 
+import scala.collection.mutable.ListBuffer
+
 @RunWith(classOf[JUnitRunner])
 class PlayWSJsonResponseSpec extends FlatSpec with MockitoSugar {
   behavior of "results"
 
-  it should "just handle chunked response of query with 100 points" in {
+  it should "handle success response of a query with 100 points" in {
     // Prepare
     val response = mock[WSResponse]
     when(response.status).thenReturn(Status.OK)
@@ -38,12 +42,67 @@ class PlayWSJsonResponseSpec extends FlatSpec with MockitoSugar {
     verify(response).status
     verifyNoMoreInteractions(response)
   }
+
+  it should "handle error response" in {
+    // Prepare
+    val response = mock[WSResponse]
+    when(response.status).thenReturn(Status.NOT_FOUND)
+    when(response.json).thenReturn(Json.parse(PlayWSJsonResponseSpec.errorData1).as[JsObject])
+
+    // Execute
+    val testJsonResponse = new TestJsonResponse(response)
+    val ex = intercept[ReactiveInfluxException] {
+      testJsonResponse.result
+    }
+
+    // Assert
+    assert(ex.getMessage.startsWith("Not a successful response!"))
+    assert(testJsonResponse.errors.size == 1)
+    assert(testJsonResponse.errors.head.message == "wtf")
+
+    // Verify
+    verify(response).json
+    verify(response).status
+    verifyNoMoreInteractions(response)
+  }
+
+  it should "handle success response with an error" in {
+    // Prepare
+    val response = mock[WSResponse]
+    when(response.status).thenReturn(Status.OK)
+    when(response.json).thenReturn(Json.parse(PlayWSJsonResponseSpec.errorData2).as[JsObject])
+
+    // Execute
+    val testJsonResponse = new TestJsonResponse(response)
+    val result = testJsonResponse.result
+
+    // Assert
+    assert(result.size == 1)
+    assert((result.head \ "error").get == JsString("hmm"))
+    assert(testJsonResponse.errors.size == 1)
+    assert(testJsonResponse.errors.head.message == "hmm")
+
+    // Verify
+    verify(response).json
+    verify(response).status
+    verifyNoMoreInteractions(response)
+  }
 }
 
 object PlayWSJsonResponseSpec {
   val data1 =
     """{"results":[{"series":[{"name":"m1","columns":["time","fk"],"values":[["1983-01-10T11:42:08.013Z",-1],["1983-01-10T11:42:09.013Z",-1],["1983-01-10T11:42:10.013Z",-1],["1983-01-10T11:42:11.013Z",-1],["1983-01-10T11:42:12.013Z",-1],["1983-01-10T11:42:13.013Z",-1],["1983-01-10T11:42:14.013Z",-1],["1983-01-10T11:42:15.013Z",-1],["1983-01-10T11:42:16.013Z",-1],["1983-01-10T11:42:17.013Z",-1],["1983-01-10T11:42:18.013Z",-1],["1983-01-10T11:42:19.013Z",-1],["1983-01-10T11:42:20.013Z",-1],["1983-01-10T11:42:21.013Z",-1],["1983-01-10T11:42:22.013Z",-1],["1983-01-10T11:42:23.013Z",-1],["1983-01-10T11:42:24.013Z",-1],["1983-01-10T11:42:25.013Z",-1],["1983-01-10T11:42:26.013Z",-1],["1983-01-10T11:42:27.013Z",-1],["1983-01-10T11:42:28.013Z",-1],["1983-01-10T11:42:29.013Z",-1],["1983-01-10T11:42:30.013Z",-1],["1983-01-10T11:42:31.013Z",-1],["1983-01-10T11:42:32.013Z",-1],["1983-01-10T11:42:33.013Z",-1],["1983-01-10T11:42:34.013Z",-1],["1983-01-10T11:42:35.013Z",-1],["1983-01-10T11:42:36.013Z",-1],["1983-01-10T11:42:37.013Z",-1],["1983-01-10T11:42:38.013Z",-1],["1983-01-10T11:42:39.013Z",-1],["1983-01-10T11:42:40.013Z",-1],["1983-01-10T11:42:41.013Z",-1],["1983-01-10T11:42:42.013Z",-1],["1983-01-10T11:42:43.013Z",-1],["1983-01-10T11:42:44.013Z",-1],["1983-01-10T11:42:45.013Z",-1],["1983-01-10T11:42:46.013Z",-1],["1983-01-10T11:42:47.013Z",-1],["1983-01-10T11:42:48.013Z",-1],["1983-01-10T11:42:49.013Z",-1],["1983-01-10T11:42:50.013Z",-1],["1983-01-10T11:42:51.013Z",-1],["1983-01-10T11:42:52.013Z",-1],["1983-01-10T11:42:53.013Z",-1],["1983-01-10T11:42:54.013Z",-1],["1983-01-10T11:42:55.013Z",-1],["1983-01-10T11:42:56.013Z",-1],["1983-01-10T11:42:57.013Z",-1],["1983-01-10T11:42:58.013Z",-1],["1983-01-10T11:42:59.013Z",-1],["1983-01-10T11:43:00.013Z",-1],["1983-01-10T11:43:01.013Z",-1],["1983-01-10T11:43:02.013Z",-1],["1983-01-10T11:43:03.013Z",-1],["1983-01-10T11:43:04.013Z",-1],["1983-01-10T11:43:05.013Z",-1],["1983-01-10T11:43:06.013Z",-1],["1983-01-10T11:43:07.013Z",-1],["1983-01-10T11:43:08.013Z",-1],["1983-01-10T11:43:09.013Z",-1],["1983-01-10T11:43:10.013Z",-1],["1983-01-10T11:43:11.013Z",-1],["1983-01-10T11:43:12.013Z",-1],["1983-01-10T11:43:13.013Z",-1],["1983-01-10T11:43:14.013Z",-1],["1983-01-10T11:43:15.013Z",-1],["1983-01-10T11:43:16.013Z",-1],["1983-01-10T11:43:17.013Z",-1],["1983-01-10T11:43:18.013Z",-1],["1983-01-10T11:43:19.013Z",-1],["1983-01-10T11:43:20.013Z",-1],["1983-01-10T11:43:21.013Z",-1],["1983-01-10T11:43:22.013Z",-1],["1983-01-10T11:43:23.013Z",-1],["1983-01-10T11:43:24.013Z",-1],["1983-01-10T11:43:25.013Z",-1],["1983-01-10T11:43:26.013Z",-1],["1983-01-10T11:43:27.013Z",-1],["1983-01-10T11:43:28.013Z",-1],["1983-01-10T11:43:29.013Z",-1],["1983-01-10T11:43:30.013Z",-1],["1983-01-10T11:43:31.013Z",-1],["1983-01-10T11:43:32.013Z",-1],["1983-01-10T11:43:33.013Z",-1],["1983-01-10T11:43:34.013Z",-1],["1983-01-10T11:43:35.013Z",-1],["1983-01-10T11:43:36.013Z",-1],["1983-01-10T11:43:37.013Z",-1],["1983-01-10T11:43:38.013Z",-1],["1983-01-10T11:43:39.013Z",-1],["1983-01-10T11:43:40.013Z",-1],["1983-01-10T11:43:41.013Z",-1],["1983-01-10T11:43:42.013Z",-1],["1983-01-10T11:43:43.013Z",-1],["1983-01-10T11:43:44.013Z",-1],["1983-01-10T11:43:45.013Z",-1],["1983-01-10T11:43:46.013Z",-1],["1983-01-10T11:43:47.013Z",-1]]}]}]}"""
+
+  val errorData1 = """{"error": "wtf"}"""
+  val errorData2 = """{"results": [{"error": "hmm"}]}"""
 }
 private class TestJsonResponse(wsResponse: WSResponse) extends PlayWSJsonResponse[Seq[JsObject]](wsResponse) {
+  val errors = new ListBuffer[ReactiveInfluxError]
+
+  override val errorHandler: PartialFunction[ReactiveInfluxError, Option[ReactiveInfluxError]] = {
+    case error =>
+      errors += error
+      None
+  }
   override def result = results
 }
